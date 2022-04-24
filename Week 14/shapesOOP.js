fa898372f21b6ee9c7c73dc5a1ab7d79d9faccc4
Chloe Hill <98047961+achloehill@users.noreply.github.com>
2022-04-24 13:42:35 -0600

    Add files via upload
    Not Finished; Fixing Issues

---
+++ Week 14/shapesOOP.js
@@ -0,0 +1,20 @@
+class Rectangle
+{
+    //x=x coord.,y=y coord., w=width, h=height, r=red, g=green, b=blue
+    constructor(x,y,w,h,r,g,b)
+    {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    display () 
+    {
+        fill(this.r, this.g, this.b);
+        rect(this.x, this.y, this.w, this.h);
+    }
+}
